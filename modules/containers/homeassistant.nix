e7ca9b8b43fff518c116d2466a377a9611d3828c
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-03-23 09:55:34 +0100

    chore: automatic commit before deployment

--- modules/containers/homeassistant.nix
+++ modules/containers/homeassistant.nix
@@ -26,7 +26,7 @@ _: {
         image = "ghcr.io/home-assistant/home-assistant:stable"; # Warning: if the tag does not change, the image will not be updated
         extraOptions = [
           "--network=host"
-          "--device=/dev/ttyACM0:/dev/ttyACM0" # Example, change this to match your own hardware
+          # "--device=/dev/ttyACM0:/dev/ttyACM0" # Example, change this to match your own hardware
         ];
       };
     };
