3f6762db4fabfd700a750b6ebe670ca54c05076b
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-03-23 10:37:23 +0100

    chore: automatic commit before deployment

--- modules/services/unifi.nix
+++ modules/services/unifi.nix
@@ -1,15 +1,13 @@
 _: {
-  flake.nixosModules.unifi =
-    { pkgs, ... }:
-    {
-      imports = [ ../../../pkgs/unifi-os-server-image/module.nix ];
+  flake.nixosModules.unifi = {pkgs, ...}: {
+    imports = [../../../pkgs/unifi-os-server-image/module.nix];
 
-      services.unifi-os-server = {
-        enable = true;
-        package = pkgs.callPackage ../../../pkgs/unifi-os-server-image {
-          sha256 = "sha256-IPoWR5GTiy7J1WgMEYdTxGo26qM2nO+U1c742pRo354=";
-        };
-        openFirewall = true;
+    services.unifi-os-server = {
+      enable = true;
+      package = pkgs.callPackage ../../../pkgs/unifi-os-server-image {
+        sha256 = "sha256-IPoWR5GTiy7J1WgMEYdTxGo26qM2nO+U1c742pRo354=";
       };
+      openFirewall = true;
     };
+  };
 }
