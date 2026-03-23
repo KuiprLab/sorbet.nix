595bdb5812ba1d05bcb42d0962ae5bfbef79f4a0
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-03-23 11:16:49 +0100

    chore: automatic commit before deployment

---
+++ modules/services/gh_runners/default.nix
@@ -0,0 +1,24 @@
+_: {
+  flake.nixosModules.caddy = {
+    pkgs,
+    config,
+    ...
+  }: {
+    sops.secrets = {
+      "token" = {
+        sopsFile = ./token;
+        format = "binary";
+        key = "";
+      };
+    };
+
+    services.github-runners = {
+      deploy = {
+        enable = true;
+        name = "deploy-runner";
+        tokenFile = "${config.sops.secrets.token.sopsFile}";
+        url = "https://github.com/KuiprLab/sorbet.nix";
+      };
+    };
+  };
+}
