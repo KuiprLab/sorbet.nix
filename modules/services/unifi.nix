dca7b13831db74409cf6978c7eabc3b14c349c8c
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-03-23 10:37:14 +0100

    fix?

---
+++ modules/services/unifi.nix
@@ -0,0 +1,15 @@
+_: {
+  flake.nixosModules.unifi =
+    { pkgs, ... }:
+    {
+      imports = [ ../../../pkgs/unifi-os-server-image/module.nix ];
+
+      services.unifi-os-server = {
+        enable = true;
+        package = pkgs.callPackage ../../../pkgs/unifi-os-server-image {
+          sha256 = "sha256-IPoWR5GTiy7J1WgMEYdTxGo26qM2nO+U1c742pRo354=";
+        };
+        openFirewall = true;
+      };
+    };
+}
