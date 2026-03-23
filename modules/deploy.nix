04244463afb6394a903c7643a6dc84c769c425e4
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-03-23 09:31:52 +0100

    chore: automatic commit before deployment

--- modules/deploy.nix
+++ modules/deploy.nix
@@ -12,6 +12,7 @@
         inherit user;
         sshUser = user;
         remoteBuild = true;
+        sudo = "doas -u";
         interactiveSudo = true;
         path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.sorbet;
       };
