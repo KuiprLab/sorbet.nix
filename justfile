72f195b5fd579c38cce41180a2cb510f5405eaf9
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-03-23 09:28:39 +0100

    chore: automatic commit before deployment

--- justfile
+++ justfile
@@ -22,7 +22,7 @@ install target:
       --option extra-trusted-public-keys cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM= \
       --target-host {{target}} 
 
-deploy: 
+deploy: fmt
   @./scripts/deploy.sh {{config}}
 
 
--- modules/deploy.nix
+++ modules/deploy.nix
@@ -2,22 +2,19 @@
   self,
   inputs,
   ...
-}:
-{
-  flake.deploy.nodes =
-    let
-      user = "daniel";
-    in
-    {
-      sorbet = {
-        hostname = "192.168.0.85";
-        profiles.system = {
-          inherit user;
-          sshUser = user;
-          remoteBuild = true;
-          interactiveSudo = false;
-          path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.sorbet;
-        };
+}: {
+  flake.deploy.nodes = let
+    user = "daniel";
+  in {
+    sorbet = {
+      hostname = "192.168.0.85";
+      profiles.system = {
+        inherit user;
+        sshUser = user;
+        remoteBuild = true;
+        interactiveSudo = false;
+        path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.sorbet;
       };
     };
+  };
 }
