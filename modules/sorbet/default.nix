6bffa0671d80fa95593c099ddf972bc312004338
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-03-23 09:46:59 +0100

    chore: automatic commit before deployment

--- modules/sorbet/default.nix
+++ modules/sorbet/default.nix
@@ -18,34 +18,41 @@
   # Helper to collect all modules from an attrset
   collectModules = attrs: lib.attrValues (lib.filterAttrs (_n: v: v != {}) attrs);
 in {
-  flake.nixosConfigurations.sorbet = inputs.nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-    modules =
-      [
-        self.nixosModules.sorbetConfiguration
-        # Core modules
-        inputs.home-manager.nixosModules.home-manager
-        inputs.determinate.nixosModules.default
-        # Nixpkgs configuration
-        {
-          nixpkgs.config = nixpkgsConfig;
-          nixpkgs.overlays = overlays;
-        }
+  flake = {
+    modules = {
+      nixosModules = {};
+      homeManagerModules = {};
+    };
 
-        # Home Manager shared modules
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            backupFileExtension = "backup";
-            sharedModules =
-              [
-                inputs.sops-nix.homeManagerModules.sops
-              ]
-              ++ collectModules self.homeManagerModules;
-          };
-        }
-      ]
-      ++ collectModules self.nixosModules;
+    nixosConfigurations.sorbet = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        [
+          self.nixosModules.sorbetConfiguration
+          # Core modules
+          inputs.home-manager.nixosModules.home-manager
+          inputs.determinate.nixosModules.default
+          # Nixpkgs configuration
+          {
+            nixpkgs.config = nixpkgsConfig;
+            nixpkgs.overlays = overlays;
+          }
+
+          # Home Manager shared modules
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              backupFileExtension = "backup";
+              sharedModules =
+                [
+                  inputs.sops-nix.homeManagerModules.sops
+                ]
+                ++ collectModules self.homeManagerModules;
+            };
+          }
+        ]
+        ++ collectModules self.nixosModules;
+    };
   };
 }
