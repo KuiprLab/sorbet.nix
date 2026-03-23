e33d0368adefb7f16bb6a28483f0c61cb3016933
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-03-23 10:50:12 +0100

    chore: automatic commit before deployment

--- modules/services/musicassistant.nix
+++ modules/services/musicassistant.nix
@@ -1,26 +1,9 @@
 _: {
   # https://nixos.wiki/wiki/Home_Assistant#NixOS_Module
   flake.nixosModules.musicassistant = _: {
-    networking.firewall = {
-      allowedTCPPorts = [ 8123 ];
-
-      allowedTCPPortRanges = [
-        {
-          from = 100;
-          to = 65535;
-        }
-      ];
-      allowedUDPPortRanges = [
-        {
-          from = 100;
-          to = 65535;
-        }
-      ];
-    };
-
     virtualisation.oci-containers = {
       containers.musicassistant = {
-        volumes = [ "music-assistant:/data" ];
+        volumes = ["music-assistant:/data"];
         environment.TZ = "Europe/Berlin";
         image = "ghcr.io/music-assistant/server:latest"; # Warning: if the tag does not change, the image will not be updated
         environment = {
