a34f20c92bbdbee12243eae0a87385005b95a883
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-03-24 09:39:56 +0100

    chore: automatic commit before deployment

--- modules/services/unifi.nix
+++ modules/services/unifi.nix
@@ -2,21 +2,6 @@ _: {
   flake.nixosModules.unifi = {pkgs, ...}: {
     imports = [../../pkgs/unifi-os-server-image/module.nix];
 
-    networking.firewall = {
-      allowedTCPPorts = [
-        443 # HTTPS portal
-        8080 # UAP device inform
-        8443 # Controller HTTPS
-        8843 # HTTPS portal redirect
-        8880 # HTTP portal redirect
-        6789 # Mobile speed test
-      ];
-      allowedUDPPorts = [
-        3478 # STUN
-        10001 # Device discovery
-      ];
-    };
-
     services.unifi-os-server = {
       enable = true;
       package = pkgs.callPackage ../../pkgs/unifi-os-server-image {
