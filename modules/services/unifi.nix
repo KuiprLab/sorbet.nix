cb14b9f78a351ea6d969949cb843adf1ccdff2c0
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-03-24 09:15:02 +0100

    fix: set inform IP

--- modules/services/unifi.nix
+++ modules/services/unifi.nix
@@ -1,29 +1,34 @@
 _: {
-  flake.nixosModules.unifi = {pkgs, ...}: {
-    imports = [../../pkgs/unifi-os-server-image/module.nix];
+  flake.nixosModules.unifi =
+    { pkgs, ... }:
+    {
+      imports = [ ../../pkgs/unifi-os-server-image/module.nix ];
 
-    networking.firewall = {
-      allowedTCPPorts = [
-        443 # HTTPS portal
-        8080 # UAP device inform
-        8443 # Controller HTTPS
-        8843 # HTTPS portal redirect
-        8880 # HTTP portal redirect
-        6789 # Mobile speed test
-      ];
-      allowedUDPPorts = [
-        3478 # STUN
-        10001 # Device discovery
-      ];
-    };
-
-    services.unifi-os-server = {
-      enable = true;
-      package = pkgs.callPackage ../../pkgs/unifi-os-server-image {
-        sha256 = "sha256-IPoWR5GTiy7J1WgMEYdTxGo26qM2nO+U1c742pRo354=";
+      networking.firewall = {
+        allowedTCPPorts = [
+          443 # HTTPS portal
+          8080 # UAP device inform
+          8443 # Controller HTTPS
+          8843 # HTTPS portal redirect
+          8880 # HTTP portal redirect
+          6789 # Mobile speed test
+        ];
+        allowedUDPPorts = [
+          3478 # STUN
+          10001 # Device discovery
+        ];
       };
-      imageTag = "uosserver:0.0.54";
-      openFirewall = true;
+
+      services.unifi-os-server = {
+        enable = true;
+        package = pkgs.callPackage ../../pkgs/unifi-os-server-image {
+          sha256 = "sha256-IPoWR5GTiy7J1WgMEYdTxGo26qM2nO+U1c742pRo354=";
+        };
+        imageTag = "uosserver:0.0.54";
+        openFirewall = true;
+        environment = {
+          UOS_SYSTEM_IP = "192.168.0.85";
+        };
+      };
     };
-  };
 }
