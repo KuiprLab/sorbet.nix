2969225a86dfc8b9ae45ce3538b31d5ce67a9d5b
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-03-25 12:48:01 +0100

    chore: automatic commit before deployment

--- modules/services/homeassistant.nix
+++ modules/services/homeassistant.nix
@@ -19,6 +19,12 @@ _: {
 
     # https://nixos.wiki/wiki/Home_Assistant#NixOS_Module
     nixosModules.homeassistant = _: {
+      networking.firewall.allowedTCPPorts = [
+        85353
+        1900
+        51827
+      ];
+
       virtualisation.oci-containers = {
         backend = "podman";
         containers.homeassistant = {
