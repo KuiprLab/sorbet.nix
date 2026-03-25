4b5bf0e4777a3e1583782534b349c36103f97f94
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-03-25 09:54:33 +0100

    chore: automatic commit before deployment

--- modules/services/musicassistant.nix
+++ modules/services/musicassistant.nix
@@ -1,7 +1,6 @@
 _: {
   # https://nixos.wiki/wiki/Home_Assistant#NixOS_Module
   flake.nixosModules.musicassistant = _: {
-
     flake.caddyVirtualHosts."musicassistant.lan" = ''
       reverse_proxy localhost:8095
       tls internal
@@ -9,7 +8,7 @@ _: {
 
     virtualisation.oci-containers = {
       containers.musicassistant = {
-        volumes = [ "music-assistant:/data" ];
+        volumes = ["music-assistant:/data"];
         environment.TZ = "Europe/Berlin";
         image = "ghcr.io/music-assistant/server:latest"; # Warning: if the tag does not change, the image will not be updated
         environment = {
