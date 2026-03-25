a51e2eb97d6696501390dd62d3166ae4b07d2954
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-03-25 10:34:15 +0100

    chore: automatic commit before deployment

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -17,11 +17,29 @@
   in
     _: {
       # caddy redirects to 443 automagically
-      networking.firewall.allowedTCPPorts = [80 443];
+      networking.firewall.allowedTCPPorts = [
+        80
+        443
+      ];
 
       services.caddy = {
         enable = true;
         virtualHosts = lib.mapAttrs (_: extraConfig: {inherit extraConfig;}) virtualHosts;
+        globalConfig = ''
+          pki {
+            ca local {
+              name "Sorbet Local CA"
+              root_cn "Sorbet Local CA Root"
+              intermediate_cn "Sorbet Local CA Intermediate"
+              root {
+                lifetime 87600h  # 10 years
+              }
+              intermediate {
+                lifetime 43800h  # 5 years
+              }
+            }
+          }
+        '';
       };
     };
 }
