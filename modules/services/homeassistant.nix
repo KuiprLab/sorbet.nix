130073abb1c6654a4c0b942b77e93885d3f03676
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-03-25 12:56:32 +0100

    chore: automatic commit before deployment

--- modules/services/homeassistant.nix
+++ modules/services/homeassistant.nix
@@ -3,10 +3,6 @@ _: {
     caddyVirtualHosts."homeassistant.lan" = ''
       reverse_proxy localhost:8123
       tls internal
-      header_up Host {host}
-      header_up X-Real-IP {remote_host}
-      header_up X-Forwarded-For {remote_host}
-      header_up X-Forwarded-Proto {scheme}
     '';
 
     gatusEndpoints = [
