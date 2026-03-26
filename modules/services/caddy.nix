9dbd02968fb1dfbca42c60131ba5f8db6412a357
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-03-26 14:20:40 +0100

    chore: automatic commit before deployment

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -36,7 +36,7 @@
           hash = "sha256-SbhLINpMjh9YJ5J9FC6gqa+Bz1Yh41X+53qTHgJyOOY=";
         };
         globalConfig = ''
-          acme_dns bunny { api_key {env.BUNNY_API_KEY} }
+          acme_dns bunny {env.BUNNY_API_KEY}
         '';
         virtualHosts = lib.mapAttrs (_: extraConfig: {inherit extraConfig;}) virtualHosts;
       };
