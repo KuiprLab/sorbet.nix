5d7267837c10b29799009736a7ac71f27e81ba7d
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-04-25 15:57:28 +0200

    chore: automatic commit before deployment

--- modules/services/music/_navidrome.nix
+++ modules/services/music/_navidrome.nix
@@ -9,7 +9,10 @@
   inboxFolder,
   plugins,
   ...
-}: {
+}: let
+  playlistsDir = "/var/lib/navidrome/playlists";
+  playlistFiles = builtins.attrNames (builtins.readDir ./playlists);
+in {
   systemd = {
     services.navidrome.serviceConfig = {
       BindReadOnlyPaths = [musicFolder];
@@ -19,17 +22,20 @@
     tmpfiles.rules =
       [
         "d ${pluginDir} 0750 navidrome navidrome - -"
+        "d ${playlistsDir} 0750 navidrome navidrome - -"
         "d ${homeDir}/backups 0750 daniel daniel - -"
         "d ${musicFolder} 0775 daniel music - -"
         "d ${inboxFolder} 0775 daniel daniel - -"
       ]
-      ++ map (p: "L+ ${pluginDir}/${p.name}.ndp - - - - ${p.pkg}") plugins;
+      ++ map (p: "L+ ${pluginDir}/${p.name}.ndp - - - - ${p.pkg}") plugins
+      ++ map (f: "L+ ${playlistsDir}/${f} - - - - ${./playlists/${f}}") playlistFiles;
   };
 
   services.navidrome = {
     enable = true;
     settings = {
       MusicFolder = musicFolder;
+      PlaylistsPath = playlistsDir;
       "Plugins.Enabled" = true;
       "Backup.Path" = "/var/lib/navidrome/backups";
       "Backup.Count" = 7;
