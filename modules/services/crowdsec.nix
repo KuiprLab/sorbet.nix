332998c02c8e80ca54a87b4a4c8ac86ad947630a
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-04-26 09:42:01 +0200

    fix

--- modules/services/crowdsec.nix
+++ modules/services/crowdsec.nix
@@ -7,30 +7,30 @@
 # broken module architecture (config.yaml symlink, DynamicUser issues, etc.)
 _: {
   flake.eclairNixosModules.crowdsec = _: {
-    services.crowdsec = {
-      enable = true;
-
-      hub.collections = [
-        "crowdsecurity/linux"
-        "crowdsecurity/haproxy"
-      ];
-
-      # Acquisitions: watch haproxy and sshd journals for attack signals.
-      settings.acquisitions = [
-        {
-          source = "journalctl";
-          journalctl_filter = ["-u" "haproxy.service"];
-          labels.type = "haproxy";
-        }
-        {
-          source = "journalctl";
-          journalctl_filter = ["-u" "sshd.service"];
-          labels.type = "syslog";
-        }
-      ];
-    };
+    # services.crowdsec = {
+    #   enable = true;
+    #
+    #   hub.collections = [
+    #     "crowdsecurity/linux"
+    #     "crowdsecurity/haproxy"
+    #   ];
+    #
+    #   # Acquisitions: watch haproxy and sshd journals for attack signals.
+    #   settings.acquisitions = [
+    #     {
+    #       source = "journalctl";
+    #       journalctl_filter = ["-u" "haproxy.service"];
+    #       labels.type = "haproxy";
+    #     }
+    #     {
+    #       source = "journalctl";
+    #       journalctl_filter = ["-u" "sshd.service"];
+    #       labels.type = "syslog";
+    #     }
+    #   ];
+    # };
 
     # nftables required for the firewall bouncer
-    networking.nftables.enable = true;
+    # networking.nftables.enable = true;
   };
 }
