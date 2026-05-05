1a5fe7c646ba7ee11a15455f02cb34b71674f999
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-05-05 15:17:22 +0200

    chore: automatic commit before deployment

--- modules/services/music/explo.nix
+++ modules/services/music/explo.nix
@@ -27,8 +27,8 @@ _: {
               "WEEKLY_JAMS_SCHEDULE" = "30 00 * * 1"; # Runs weekly, every Monday 30 minutes past midnight
               "WEEKLY_JAMS_FLAGS" = "--playlist=weekly-jams";
 
-              "DAILY_JAMS_SCHEDULE" = "15 01 * * *"; # Runs daily, every day 15 minutes past 1PM
-              "DAILY_JAMS_FLAGS" = "--playlist=daily-jams";
+              # "DAILY_JAMS_SCHEDULE" = "15 01 * * *"; # Runs daily, every day 15 minutes past 1PM
+              # "DAILY_JAMS_FLAGS" = "--playlist=daily-jams";
               "EXECUTE_ON_START" = "true";
             };
             labels = {
