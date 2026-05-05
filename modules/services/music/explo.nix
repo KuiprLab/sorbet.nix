83825c345f1889c36aad8913e2896e6b1922c8ae
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-05-05 15:37:37 +0200

    chore: automatic commit before deployment

--- modules/services/music/explo.nix
+++ modules/services/music/explo.nix
@@ -1,7 +1,7 @@
 _: {
   flake = {
     caddyVirtualHosts = {
-      "exploy.int.kuipr.de" = ''
+      "explo.int.kuipr.de" = ''
         reverse_proxy localhost:7288
       '';
     };
@@ -28,16 +28,6 @@ _: {
             ports = [
               "7288:7288"
             ];
-            environment = {
-              "WEEKLY_EXPLORATION_SCHEDULE" = "15 00 * * 2"; # Runs weekly, every Tuesday 15 minutes past midnight
-
-              "WEEKLY_JAMS_SCHEDULE" = "30 00 * * 1"; # Runs weekly, every Monday 30 minutes past midnight
-              "WEEKLY_JAMS_FLAGS" = "--playlist=weekly-jams";
-
-              # "DAILY_JAMS_SCHEDULE" = "15 01 * * *"; # Runs daily, every day 15 minutes past 1PM
-              # "DAILY_JAMS_FLAGS" = "--playlist=daily-jams";
-              "EXECUTE_ON_START" = "true";
-            };
             labels = {
               "io.containers.autoupdate" = "registry";
             };
