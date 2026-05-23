9c17cb038f3ba24f6cea8b95c3ee57a57997a264
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-05-23 18:08:17 +0200

    chore: automatic commit before deployment

--- modules/hosts/sorbet/default.nix
+++ modules/hosts/sorbet/default.nix
@@ -23,7 +23,7 @@ in {
     homeManagerModules = {};
 
     nixosConfigurations.sorbet = inputs.nixpkgs.lib.nixosSystem {
-      stdenv.hostPlatform.system = "x86_64-linux";
+      system = "x86_64-linux";
       modules =
         [
           self.nixosModules.sorbetConfiguration
