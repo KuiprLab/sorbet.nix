da1aa077009f8ec01b29be83e2b19d217ae56b14
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-05-30 11:02:43 +0200

    chore: use docker

--- modules/services/music/beetroot.nix
+++ modules/services/music/beetroot.nix
@@ -6,20 +6,29 @@ _: {
       '';
     };
 
-    nixosModules.beetroot = {config, ...}: {
-      sops.secrets = {
-        "beetroot" = {
-          sopsFile = ../../../secrets/sorbet/beetroot.yaml;
-          format = "yaml";
-          key = "";
+    nixosModules.beetroot =
+      { config, ... }:
+      {
+        sops.secrets = {
+          "beetroot" = {
+            sopsFile = ../../../secrets/sorbet/beetroot.yaml;
+            format = "yaml";
+            key = "";
+          };
+        };
+
+        virtualisation.oci-containers.containers.beetroot-v2 = {
+          image = "ghcr.io/you/beetroot:latest";
+          ports = [ "7290:3000" ];
+          volumes = [
+            "/home/daniel/beetroot:/data"
+            "/media/data/music/beetroot:/music"
+          ];
+          environment = {
+            NODE_ENV = "production";
+            CONFIG_PATH = config.sops.secrets."beetroot".path;
+          };
         };
       };
-
-      services.beetroot-v2 = {
-        enable = true;
-        port = 7290;
-        configFile = config.sops.secrets."beetroot".path;
-      };
-    };
   };
 }
