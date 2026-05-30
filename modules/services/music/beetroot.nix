2b906d81821d82d8820123d2370efd408ca6aae8
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-05-30 11:06:04 +0200

    chore: automatic commit before deployment

--- modules/services/music/beetroot.nix
+++ modules/services/music/beetroot.nix
@@ -6,29 +6,27 @@ _: {
       '';
     };
 
-    nixosModules.beetroot =
-      { config, ... }:
-      {
-        sops.secrets = {
-          "beetroot" = {
-            sopsFile = ../../../secrets/sorbet/beetroot.yaml;
-            format = "yaml";
-            key = "";
-          };
-        };
-
-        virtualisation.oci-containers.containers.beetroot-v2 = {
-          image = "ghcr.io/you/beetroot:latest";
-          ports = [ "7290:3000" ];
-          volumes = [
-            "/home/daniel/beetroot:/data"
-            "/media/data/music/beetroot:/music"
-          ];
-          environment = {
-            NODE_ENV = "production";
-            CONFIG_PATH = config.sops.secrets."beetroot".path;
-          };
+    nixosModules.beetroot = {config, ...}: {
+      sops.secrets = {
+        "beetroot" = {
+          sopsFile = ../../../secrets/sorbet/beetroot.yaml;
+          format = "yaml";
+          key = "";
         };
       };
+
+      virtualisation.oci-containers.containers.beetroot-v2 = {
+        image = "ghcr.io/you/beetroot:latest";
+        ports = ["7290:3000"];
+        volumes = [
+          "/home/daniel/beetroot:/data"
+          "/media/data/music/beetroot:/music"
+        ];
+        environment = {
+          NODE_ENV = "production";
+          CONFIG_PATH = config.sops.secrets."beetroot".path;
+        };
+      };
+    };
   };
 }
