8524f3d824815391418f1c89a794ad8777629c7b
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-09-02 11:34:00 +0200

    chore: automatic commit before deployment

--- modules/services/music/koito.nix
+++ modules/services/music/koito.nix
@@ -75,7 +75,7 @@ _: {
         subtidal = {
           image = "ghcr.io/frostplexx/subtidal:latest";
           volumes = [
-            "subtidal-data:/data"
+            "/home/daniel/subtidal:/data"
             "${config.sops.secrets."subtidal".path}:/config/subtidal/settings.toml:ro"
           ];
           ports = ["4234:8000"];
